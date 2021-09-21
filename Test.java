c7dc29e506f996568ee2c075ca889f8bf9672524
zifeiyu0531 <32443516+zifeiyu0531@users.noreply.github.com>
2021-09-21 16:11:34 +0800

    测试

---
+++ Test.java
@@ -0,0 +1,39 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+public class Test {
+    public static void main(String[] args) {
+        BloomFilter<String> bloomFilter = new BloomFilter.Builder()
+                                                .setElementNums(1000)
+                                                .setFalsePositiveRate(0.01)
+                                                .build();
+        bloomFilter.add("abc");
+        System.out.println(bloomFilter.contains("abc"));
+        System.out.println(bloomFilter.contains("ccc"));
+        System.out.println(bloomFilter.count());
+        bloomFilter.reset();
+        System.out.println(bloomFilter.count());
+
+        BloomFilter<Integer> bloomFilterInt = new BloomFilter.Builder()
+                                                .setElementNums(1000)
+                                                .setFalsePositiveRate(0.1)
+                                                .build();
+        Random r = new Random();
+        Map<Integer, Boolean> map = new HashMap<>();
+        for(int i = 0; i < 1000; i++){
+            int rand = r.nextInt(10000);
+            map.put(rand, true);
+            bloomFilterInt.add(rand);
+        }
+        System.out.println(String.format("Bloom Filter has %d elements now", bloomFilterInt.count()));
+        int falsePositiveCount = 0;
+        for(int i = 0; i < 100000; i++){
+            int rand = r.nextInt(10000);
+            if(!map.keySet().contains(rand) && bloomFilterInt.contains(rand)){
+                falsePositiveCount++;
+            }
+        }
+        System.out.println(String.format("After 100000 tests, has %d false positive, the rate is %f", falsePositiveCount, ((float)falsePositiveCount) / 100000));
+    }
+}
